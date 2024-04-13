e0c792cb84b6fd0bed1d2c01d4d8c6800f1dc207
codemob <69110900+commandblox@users.noreply.github.com>
2024-04-13 13:47:32 -0400

    pain

--- index.js
+++ index.js
@@ -17,19 +17,39 @@ document.addEventListener("DOMContentLoaded", event=>{
         let distanceX = buttonX - mouseData.x;
         let distanceY = buttonY - mouseData.y;
         let distance = Math.sqrt(Math.pow(distanceX,2)+Math.pow(distanceY,2));
-        let moveMultiplier = Math.pow(6/distance, 2)*10;
+        let moveMultiplier = Math.pow(7/distance, 2)*3;
 
-        buttonX += clamp(moveMultiplier*distanceX, -7.5, 7.5);
-        buttonY += clamp(moveMultiplier*distanceY, -7.5, 7.5);
+        buttonX += clamp(moveMultiplier*distanceX, -3, 3);
+        buttonY += clamp(moveMultiplier*distanceY, -3, 3);
 
         buttonY = clamp(buttonY, 20, window.innerHeight  - 20);
         buttonX = clamp(buttonX, 60, window.innerWidth   - 60);
 
         nextButton.style.left = buttonX/window.innerWidth * 100 + 'vw';
         nextButton.style.top  = buttonY/window.innerHeight * 100 + 'vh';
-    }, 1000/60);
-});
 
-function clickNext() {
-    (new Audio("pipe.mp3")).play();
-}
+
+        let backgroundX = +document.documentElement.style.backgroundPositionX.replace('%', '');
+        let backgroundY = +document.documentElement.style.backgroundPositionY.replace('%', '');
+
+        backgroundX += (mouseData.x / window.innerWidth  - .5) * -2;
+        backgroundY += (mouseData.y / window.innerHeight - .5) * 2;
+
+        document.documentElement.style.backgroundPositionX = backgroundX + '%'
+        document.documentElement.style.backgroundPositionY = backgroundY + '%'
+    }, 1000/60);
+
+    
+    var itemNum = 0;
+    clickNext = function() {
+        (new Audio("pipe.mp3")).play();
+        let item = document.getElementById('next-item'+itemNum);
+        item.style.opacity = "100%"
+        itemNum++;
+        
+        nextButton.style.left = Math.random() * 100 + 'vw';
+        nextButton.style.top  = Math.random() * 100 + 'vh';
+    }
+
+});
+var clickNext;
